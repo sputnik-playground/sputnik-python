a6bb0f58f231af81cf29970e8f4e9ae0dc5749b4
Piotr Jagielski <pjg@touk.pl>
2016-05-12 14:02:24 +0200

    test

--- DemoModule.py
+++ DemoModule.py
@@ -5,6 +5,12 @@ def nice_function(value):
     return value**2
 
 
+def bad_code(bar):
+    foo = bar*2
+    xxx = 50
+    return foo
+
 if __name__ == "__main__":
     print("Hello Sputnik!")
     print(nice_function(5))
+    print(bad_code(10)) 
